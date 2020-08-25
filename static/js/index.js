9e4117ae34ac1826d2e47b12980e65ec1b16e94c
cristianv94 <69884878+cristianv94@users.noreply.github.com>
2020-08-24 22:39:57 -0500

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -1,15 +1,20 @@
 //https://www.eclipse.org/paho/clients/js/
 
 function LED1_On() {
-	alert("led on");
+	
 	console.log("led on");
-	document.getElementById("sensor").innerHTML="led on";
+        message = new Paho.MQTT.Message("LED1_ON");
+        message.destinationName = "crisandresveloz@hotmail.com/test";
+        client.send(message);
+        
   
 }
 function LED1_Off(){	
-	alert("led off");
+	
 	console.log("led off");
-	document.getElementById("sensor").innerHTML="led off";
+        message = new Paho.MQTT.Message("LED1_OFF")
+        message.destinationName = "crisandresveloz@hotmail.com/test";
+        client.send(message);
 }
 
 
@@ -27,8 +32,8 @@ function LED1_Off(){
   client.onMessageArrived = onMessageArrived;
   var options = {
    useSSL: false,
-    userName: "lfrenteriax@hotmail.com",
-    password: "lfrenteriax",
+    userName: "crisandresveloz@hotmail.com",
+    password: "260993lduq",
     onSuccess:onConnect,
     onFailure:doFail
   }
@@ -41,9 +46,9 @@ function LED1_Off(){
     // Once a connection has been made, make a subscription and send a message.
     console.log("Conectado...");
 	
-    client.subscribe("lfrenteriax@hotmail.com/test");
+    client.subscribe("crisandresveloz@hotmail.com/test");
     message = new Paho.MQTT.Message("hola desde la web");
-    message.destinationName = "lfrenteriax@hotmail.com/test1";
+    message.destinationName = "crisandresveloz@hotmail.com/test";
     client.send(message);
 	
   }
@@ -61,8 +66,8 @@ function LED1_Off(){
   }
 
   // called when a message arrives
-function onMessageArrived(message) {
-	console.log("Nuevo mensaje:"+message.payloadString);
-	document.getElementById("sensor").innerHTML=message.payloadString.split("=")[1];
- }
+  function onMessageArrived(message) {
+    console.log("Nuevo mensaje:"+message.payloadString);
+    document.getElementById("sensor").innerHTML=message.payloadString.split("=")[1];
+  }
   
